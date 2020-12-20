23d2241992a0a9afe0c81e7ac19f348d9c4659b3
Sherlock517 <tomejerry99@gmail.com>
2020-12-21 01:11:57 +0900

    PlayerController Created

---
+++ Huma/Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,14 @@
+﻿//********************************************
+// EDITOR : KIM HanGyul
+// LAST EDITED DATE : 2020.12.21
+// Script Purpose : ...
+//********************************************
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+    
+}
