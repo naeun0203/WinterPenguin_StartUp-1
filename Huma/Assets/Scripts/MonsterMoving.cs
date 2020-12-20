7d15c00f7b55ef15a48ad930175a432598c5329c
naeun0203 <72336040+naeun0203@users.noreply.github.com>
2020-12-21 01:11:55 +0900

    MonsterMoving Created

---
+++ Huma/Assets/Scripts/MonsterMoving.cs
@@ -0,0 +1,13 @@
+﻿//************************************************
+// EDITOR : JNE
+// LAST EDITED DATE : 2020.12.21
+// Script Purpose : ***
+//******************************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMoving : MonoBehaviour
+{
+
+}
