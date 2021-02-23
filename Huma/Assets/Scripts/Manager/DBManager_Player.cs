07cdda89c74e9044adc03f97455e44fc3f712021
CorgiMuzi <49934162+CorgiMuzi@users.noreply.github.com>
2021-02-23 10:41:35 +0900

    DBManager_Player Edited

--- Huma/Assets/Scripts/Manager/DBManager_Player.cs
+++ Huma/Assets/Scripts/Manager/DBManager_Player.cs
@@ -2,7 +2,7 @@
  * 
  * EDITOR : KIM Ji hun 
  * Last Edit : 2021.2.17
- * Script Purpose : Setting Character(Player)'s Database
+ * Script Purpose : Setting character(Player)'s Database
  * 
  */
 using System.Collections;
@@ -13,7 +13,7 @@ using System;
 
 public class DBManager_Player : MonoBehaviour
 {
-    private string[] characters;
+    private string[] characterArr;
 
     public int ID; // Character ID
     public string characterName;
@@ -29,40 +29,44 @@ public class DBManager_Player : MonoBehaviour
     public int NumberOfTargets;
     public int skillCoolTime;
 
+    #region Exp
+    [HideInInspector]
+    public List<float> ExpList = new List<float>();
+    #endregion
+
     public bool isLoaded = false;
 
     public void LoadingCharacterData(int ID)
     {
         isLoaded = false;
-        StartCoroutine(DataGet("http://220.127.167.244:8080//WinterPenguin_Huma/CharactersDB.php", ID));
+        StartCoroutine(PlayerDataGet("http://220.127.167.244:8080//WinterPenguin_Huma/CharactersDB.php",ID));
 
     }
 
-    private IEnumerator DataGet(string _url, int _index)
+    private IEnumerator PlayerDataGet(string _characterUrl, int _index)
     {
-        UnityWebRequest www = UnityWebRequest.Get(_url);
-        yield return www.SendWebRequest();
-        if (www.error != null)
+        UnityWebRequest charData = UnityWebRequest.Get(_characterUrl);
+        yield return charData.SendWebRequest();
+        if (charData.error != null)
         {
-            Debug.Log(www.error.ToString());
+            Debug.Log(charData.error.ToString());
         }
 
-        characters = www.downloadHandler.text.Split(';');
+        characterArr = charData.downloadHandler.text.Split(';');
 
-        characterName = GetDataValue(characters[_index], "Name:");
-        hp = Convert.ToSingle(GetDataValue(characters[_index], "HP:"));
-        damage = Convert.ToSingle(GetDataValue(characters[_index], "Damage:"));
-        attackSpeed = Convert.ToSingle(GetDataValue(characters[_index], "AttackSpeed:"));
-        attackRange = Convert.ToInt32(GetDataValue(characters[_index], "AttackRange:"));
-        attackRadius = Convert.ToInt32(GetDataValue(characters[_index], "AttackRadius:"));
-        criticalProb = Convert.ToSingle(GetDataValue(characters[_index], "CriticalProbability:"));
-        criticalDamage = Convert.ToSingle(GetDataValue(characters[_index], "CriticalDamage:"));
-        bloodSucking = Convert.ToSingle(GetDataValue(characters[_index], "BloodSucking:"));
-        moveSpeed = Convert.ToInt32(GetDataValue(characters[_index], "MoveSpeed:"));
-        NumberOfTargets = Convert.ToInt32(GetDataValue(characters[_index], "NumberOfTargets:"));
-        skillCoolTime = Convert.ToInt32(GetDataValue(characters[_index], "SkillCoolTime:"));
+        characterName = GetDataValue(characterArr[_index], "Name:");
+        hp = Convert.ToSingle(GetDataValue(characterArr[_index], "HP:"));
+        damage = Convert.ToSingle(GetDataValue(characterArr[_index], "Damage:"));
+        attackSpeed = Convert.ToSingle(GetDataValue(characterArr[_index], "AttackSpeed:"));
+        attackRange = Convert.ToInt32(GetDataValue(characterArr[_index], "AttackRange:"));
+        attackRadius = Convert.ToInt32(GetDataValue(characterArr[_index], "AttackRadius:"));
+        criticalProb = Convert.ToSingle(GetDataValue(characterArr[_index], "CriticalProbability:"));
+        criticalDamage = Convert.ToSingle(GetDataValue(characterArr[_index], "CriticalDamage:"));
+        bloodSucking = Convert.ToSingle(GetDataValue(characterArr[_index], "BloodSucking:"));
+        moveSpeed = Convert.ToInt32(GetDataValue(characterArr[_index], "MoveSpeed:"));
+        NumberOfTargets = Convert.ToInt32(GetDataValue(characterArr[_index], "NumberOfTargets:"));
+        skillCoolTime = Convert.ToInt32(GetDataValue(characterArr[_index], "SkillCoolTime:"));
         isLoaded = true;
-
     }
 
     string GetDataValue(string data, string index)
@@ -71,5 +75,11 @@ public class DBManager_Player : MonoBehaviour
         if(value.Contains("|"))
             value = value.Remove(value.IndexOf("|"));
         return value;
+    }
+
+    private List<float> GetExpValue()
+    {
+        //ExpList = 
+        return ExpList;
     }
 }
