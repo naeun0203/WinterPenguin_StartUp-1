bd4edfe1173e134159962ee1549086af90f29117
CorgiMuzi <49934162+CorgiMuzi@users.noreply.github.com>
2021-03-02 14:45:27 +0900

    ...

--- Huma/Assets/Scripts/Monster/MonsterBase.cs
+++ Huma/Assets/Scripts/Monster/MonsterBase.cs
@@ -96,9 +96,7 @@ public class MonsterBase : MonoBehaviour
         nvAgent.stoppingDistance = AttackRange;
         yield return null;
     }
-    /// <summary>
-    /// 
-    /// </summary>
+    
     #region HP, EXP
     public float HP
     {
