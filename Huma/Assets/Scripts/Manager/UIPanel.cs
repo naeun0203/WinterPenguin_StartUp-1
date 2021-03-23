ba22d4152ee3296a750490fbf9a9807abd061adc
Kang-dahye <73806054+Kang-dahye@users.noreply.github.com>
2021-03-23 11:00:51 +0900

    UI PopUp Edited

---
+++ Huma/Assets/Scripts/Manager/UIPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPanel : MonoBehaviour
+{
+    //public bool isOpen = false;
+
+    public virtual void OpenBehavior() 
+    {
+        gameObject.SetActive(true);
+        /*        if (!isOpen)
+                {
+                    isOpen = true;
+                    gameObject.SetActive(true);
+                }*/
+    }
+
+    public virtual void UpdateBehavior()
+    {
+
+    }
+    public virtual void CloseBehavior()
+    {
+        gameObject.SetActive(false);
+/*        if (isOpen)
+        {
+            isOpen = false;
+            gameObject.SetActive(false);
+        }*/
+    }
+}
---
+++ Huma/Assets/Scripts/UI/SubMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubMenu : UIPanel
+{
+    public GameObject SettingMenu;
+    public Button continueButton, retryButton, settingButton, giveupButton;
+    public override void OpenBehavior()
+    {
+        base.OpenBehavior();
+
+        continueButton.onClick.AddListener(() => ContinueButtonClicked());
+        settingButton.onClick.AddListener(() => SettingButtonClicked());
+        
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && UI_Test.instance._currentPanel==this) 
+        { 
+            UI_Test.instance.TriggerClosePanel(this);
+            UI_Test.instance._currentPanel = UI_Test.instance.HUD;
+
+            Time.timeScale = 1;
+        }
+    }
+
+    void ContinueButtonClicked()
+    {
+        UI_Test.instance._currentPanel = UI_Test.instance.HUD;
+
+        UI_Test.instance.TriggerClosePanel(this);
+
+        Time.timeScale = 1;
+    }
+
+    void SettingButtonClicked()
+    {
+        UIPanel targetPanel = UI_Test.instance.settingMenu;
+        UI_Test.instance.TriggerOpenPanel(targetPanel);
+    }
+}
